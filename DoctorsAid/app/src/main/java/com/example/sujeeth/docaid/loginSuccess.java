643d386c8aab69605e3f2851749b82480149421c
Sujeeth <sujeeth19093@gmail.com>
2017-12-06 15:51:30 -0800

    *added pg2 and 3 without func

--- DoctorsAid/app/src/main/java/com/example/sujeeth/docaid/loginSuccess.java
+++ DoctorsAid/app/src/main/java/com/example/sujeeth/docaid/loginSuccess.java
@@ -3,14 +3,43 @@ package com.example.sujeeth.docaid;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class loginSuccess extends AppCompatActivity {
+public class loginSuccess extends AppCompatActivity implements View.OnClickListener{
+
+    Button Go,addNew;
+    EditText patientID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_success);
-
         Intent intent = getIntent();
+
+        Go = (Button)findViewById(R.id.goButton);
+        Go.setOnClickListener(this);
+        addNew = (Button)findViewById(R.id.addNewButton);
+        addNew.setOnClickListener(this);
+
+        patientID = (EditText)findViewById(R.id.patientIDTxtBox);
+        patientID.setInputType(InputType.TYPE_NUMBER_VARIATION_NORMAL);
+    }
+
+    public void onClick(View v){
+        switch(v.getId())
+        {
+            case R.id.goButton:
+                Intent goIntent = new Intent(this, patientInfo.class);
+                startActivity(goIntent);
+                break;
+            case R.id.addNewButton:
+                Intent addNewIntent = new Intent(this, addPatientInfo.class);
+                startActivity(addNewIntent);
+                break;
+        }
     }
 }
---
+++ DoctorsAid/app/src/main/java/com/example/sujeeth/docaid/patientInfo.java
@@ -0,0 +1,13 @@
+package com.example.sujeeth.docaid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class patientInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_info);
+    }
+}
