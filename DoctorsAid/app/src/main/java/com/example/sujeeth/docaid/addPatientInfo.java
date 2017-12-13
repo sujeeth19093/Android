baa5ee9aee7fae1a1116e9555c9c3406f6c37754
Sujeeth <sujeeth19093@gmail.com>
2017-12-12 16:48:55 -0800

    *Finished add patient info layout and added clear button func

--- DoctorsAid/app/src/main/java/com/example/sujeeth/docaid/addPatientInfo.java
+++ DoctorsAid/app/src/main/java/com/example/sujeeth/docaid/addPatientInfo.java
@@ -3,15 +3,59 @@ package com.example.sujeeth.docaid;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
-public class addPatientInfo extends AppCompatActivity {
+public class addPatientInfo extends AppCompatActivity implements OnClickListener{
+
+    Button Save, Clear;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_patient_info);
         Intent intent = getIntent();
 
+        Save = (Button) findViewById(R.id.saveButton);
+        Save.setOnClickListener(this);
+        Clear = (Button) findViewById(R.id.clearButton);
+        Clear.setOnClickListener(this);
+    }
+
+    private void clearFunc(ViewGroup group)
+    {
+        for(int i = 0, count = group.getChildCount(); i < count; ++i)
+        {
+            View view = group.getChildAt(i);
+
+            if(view instanceof EditText)
+            {
+                ((EditText)view).setText("");
+            }
+
+            if(view instanceof CheckBox)
+            {
+                ((CheckBox)view).setChecked(false);
+            }
+        }
+    }
+
+    public void onClick(View v)
+    {
+        switch(v.getId())
+        {
+            case R.id.saveButton:
+
+                break;
+            case R.id.clearButton:
+                clearFunc((ViewGroup) findViewById(R.id.scrollLayout));
+                break;
+        }
 
     }
 }
