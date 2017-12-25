148d8ff34be874d7bcb910b108595621a54cea0b
Sujeeth <sujeeth19093@gmail.com>
2017-12-24 16:44:43 -0800

    *new patient info to firebase basic functionality finished

--- DocHelper/app/src/main/java/com/example/sujeeth/dochelper/addPatientInfo.java
+++ DocHelper/app/src/main/java/com/example/sujeeth/dochelper/addPatientInfo.java
@@ -11,9 +11,17 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.IgnoreExtraProperties;
+
 public class addPatientInfo extends AppCompatActivity implements OnClickListener{
 
+    private DatabaseReference mDatabase;
+
     Button Save, Clear;
+    EditText Name, Age, Gender, Height, Weight, Bp_Over, Bp_Under, History;
+    CheckBox Diabetic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -22,10 +30,61 @@ public class addPatientInfo extends AppCompatActivity implements OnClickListener
         setContentView(R.layout.activity_add_patient_info);
         Intent intent = getIntent();
 
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
         Save = (Button) findViewById(R.id.saveButton);
         Save.setOnClickListener(this);
-        Clear = (Button) findViewById(R.id.clearButton);
+        Clear = (Button) findViewById(R.id.cancelButton);
         Clear.setOnClickListener(this);
+
+        Name = (EditText) findViewById(R.id.enterNameTxtBox);
+        Age = (EditText) findViewById(R.id.enterAgeTxtBox);
+        Gender = (EditText) findViewById(R.id.genderTxtBox);
+        Height = (EditText) findViewById(R.id.heightTxtBox);
+        Weight = (EditText) findViewById(R.id.weightTxtBox);
+        Bp_Over = (EditText) findViewById(R.id.upperBPTxtBox);
+        Bp_Under = (EditText) findViewById(R.id.lowerBPTxtBox);
+        History = (EditText) findViewById(R.id.patientHistoryTxtBox);
+
+        Diabetic = (CheckBox) findViewById(R.id.diabeticChkBox);
+    }
+
+    @IgnoreExtraProperties
+    public static class Patient
+    {
+        public String name, age, gender, height, weight, bp_over, bp_under, history;
+        public boolean diabetic;
+
+        public Patient()
+        {
+
+        }
+    }
+
+    private void addNewPatient(String p_name,String p_age,String p_gender,String p_height,
+                               String p_weight,String p_bp_over,String p_bp_under,String p_history, boolean p_diabetic)
+    {
+        String p_ID = p_name + "001";
+
+        Patient patient = new Patient();
+        patient.name = p_name;
+        patient.age = p_age;
+        patient.gender = p_gender;
+        patient.height = p_height;
+        patient.weight = p_weight;
+        patient.bp_over = p_bp_over;
+        patient.bp_under = p_bp_under;
+        patient.history = p_history;
+        patient.diabetic = p_diabetic;
+
+        mDatabase.child("patients").child(p_ID).setValue(patient);
+    }
+
+    private boolean validateForm()
+    {
+        boolean valid = true;
+
+        return valid;
     }
 
     private void clearFunc(ViewGroup group)
@@ -52,16 +111,27 @@ public class addPatientInfo extends AppCompatActivity implements OnClickListener
         switch(v.getId())
         {
             case R.id.saveButton:
-                myToast = Toast.makeText(getApplicationContext(),"Saved",Toast.LENGTH_SHORT);
-                myToast.show();
-                Intent saveSuccessIntent = new Intent(this, loginSuccess.class);
-                startActivity(saveSuccessIntent);
+                if(validateForm())
+                {
+                    addNewPatient(Name.getText().toString(),Age.getText().toString(),Gender.getText().toString(),Height.getText().toString(),
+                            Weight.getText().toString(),Bp_Over.getText().toString(),Bp_Under.getText().toString(),History.getText().toString(),
+                            Diabetic.isChecked());
+                    myToast = Toast.makeText(getApplicationContext(),"Saved",Toast.LENGTH_SHORT);
+                    myToast.show();
+                    clearFunc((ViewGroup) findViewById(R.id.scrollLayout));
+                    Intent loginSuccessIntent = new Intent(this, loginSuccess.class);
+                    startActivity(loginSuccessIntent);
+                }else
+                {
+
+                }
                 break;
-            case R.id.clearButton:
+            case R.id.cancelButton:
                 clearFunc((ViewGroup) findViewById(R.id.scrollLayout));
+                Intent loginSuccessIntent = new Intent(this, loginSuccess.class);
+                startActivity(loginSuccessIntent);
                 break;
         }
-
     }
 }
 
--- FirebaseTest/app/src/main/java/com/example/sujeeth/firebasetest/MainActivity.java
+++ FirebaseTest/app/src/main/java/com/example/sujeeth/firebasetest/MainActivity.java
@@ -1,37 +1,27 @@
 package com.example.sujeeth.firebasetest;
 
 import android.content.Intent;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.IgnoreExtraProperties;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements OnClickListener{
 
-    private FirebaseAuth mAuth;
-    private static final String TAG = "EmailPassword";
+    private DatabaseReference mDatabase;
 
-    Button SignInButton, SignUpButton;
-    Button SignOutButton, ContinueButton;
-    Button AuthenticateButton;
-
-    EditText usernameField, passwordField;
-    EditText signUpEmailField, signUpPasswordField;
-
-    TextView WelcomeMsg;
-    TextView AuthenticationStatus;
+    Button Save, Clear;
+    EditText Name, Age, Gender, Height, Weight, Bp_Over, Bp_Under, History;
+    CheckBox Diabetic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -39,187 +29,103 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
-        SignInButton = (Button)findViewById(R.id.signInButton);
-        SignInButton.setOnClickListener(this);
+        Save = (Button) findViewById(R.id.saveButton);
+        Save.setOnClickListener(this);
+        Clear = (Button) findViewById(R.id.clearButton);
+        Clear.setOnClickListener(this);
 
-        SignUpButton = (Button)findViewById(R.id.signUpButton);
-        SignUpButton.setOnClickListener(this);
+        Name = (EditText) findViewById(R.id.enterNameTxtBox);
+        Age = (EditText) findViewById(R.id.enterAgeTxtBox);
+        Gender = (EditText) findViewById(R.id.genderTxtBox);
+        Height = (EditText) findViewById(R.id.heightTxtBox);
+        Weight = (EditText) findViewById(R.id.weightTxtBox);
+        Bp_Over = (EditText) findViewById(R.id.upperBPTxtBox);
+        Bp_Under = (EditText) findViewById(R.id.lowerBPTxtBox);
+        History = (EditText) findViewById(R.id.patientHistoryTxtBox);
 
-        usernameField = (EditText)findViewById(R.id.usernameTBox);
-        passwordField = (EditText)findViewById(R.id.passwordTBox);
-
-        WelcomeMsg = (TextView)findViewById(R.id.welcomeTxtV);
-
-        ContinueButton = (Button)findViewById(R.id.continueButton);
-        ContinueButton.setOnClickListener(this);
-
-        SignOutButton = (Button)findViewById(R.id.signOutButton);
-        SignOutButton.setOnClickListener(this);
-
-        AuthenticateButton = (Button)findViewById(R.id.authenticateButton);
-        AuthenticateButton.setOnClickListener(this);
-
-        signUpEmailField = (EditText)findViewById(R.id.emailTBox);
-        signUpPasswordField = (EditText) findViewById(R.id.emailPWordTBox);
-
-        AuthenticationStatus = (TextView)findViewById(R.id.authenticationStatusTxtV);
-
-        mAuth = FirebaseAuth.getInstance();
+        Diabetic = (CheckBox) findViewById(R.id.diabeticChkBox);
     }
 
-    @Override
-    public void onStart()
+    @IgnoreExtraProperties
+    public static class Patient
     {
-        super.onStart();
+        public String name, age, gender, height, weight, bp_over, bp_under, history;
+        public boolean diabetic;
 
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        updateUI(currentUser);
-    }
-
-    private void createUser(String uName, String pWord)
-    {
-        Log.d(TAG, "createAccount:" + uName);
-        if(!validateForm2())
+        public Patient()
         {
-            AuthenticationStatus.setText("Error: Information is missing");
-            return;
-        }
 
-        mAuth.createUserWithEmailAndPassword(uName,pWord)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful())
-                        {
-                            Log.d(TAG, "createUserWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            updateUI(user);
-                        }else
-                        {
-                            Log.d(TAG, "createUserWithEmail:failure", task.getException());
-                            Toast.makeText(MainActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
+        }
     }
 
-    private void signIn(String uName, String pWord)
+    private void addNewPatient(String p_name,String p_age,String p_gender,String p_height,
+                               String p_weight,String p_bp_over,String p_bp_under,String p_history, boolean p_diabetic)
     {
-        Log.d(TAG, "signIn:" + uName);
-        if(!validateForm())
-        {
-            return;
-        }
+        String p_ID = p_name + "001";
 
-        mAuth.signInWithEmailAndPassword(uName, pWord)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>()
+        Patient patient = new Patient();
+        patient.name = p_name;
+        patient.age = p_age;
+        patient.gender = p_gender;
+        patient.height = p_height;
+        patient.weight = p_weight;
+        patient.bp_over = p_bp_over;
+        patient.bp_under = p_bp_under;
+        patient.history = p_history;
+        patient.diabetic = p_diabetic;
+
+        mDatabase.child("patients").child(p_ID).setValue(patient);
+    }
+
+    private boolean validateForm()
+    {
+        boolean valid = true;
+
+        return valid;
+    }
+
+    private void clearFunc(ViewGroup group)
+    {
+        for(int i = 0, count = group.getChildCount(); i < count; ++i)
+        {
+            View view = group.getChildAt(i);
+
+            if(view instanceof EditText)
+            {
+                ((EditText)view).setText("");
+            }
+
+            if(view instanceof CheckBox)
+            {
+                ((CheckBox)view).setChecked(false);
+            }
+        }
+    }
+
+    public void onClick(View v)
+    {
+        Toast myToast;
+        switch(v.getId())
+        {
+            case R.id.saveButton:
+                if(validateForm())
                 {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "signInWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            updateUI(user);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "signInWithEmail:failure", task.getException());
-                            Toast.makeText(MainActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                            updateUI(null);
-                        }
-                    }
-                });
-    }
+                    addNewPatient(Name.getText().toString(),Age.getText().toString(),Gender.getText().toString(),Height.getText().toString(),
+                            Weight.getText().toString(),Bp_Over.getText().toString(),Bp_Under.getText().toString(),History.getText().toString(),
+                            Diabetic.isChecked());
+                    myToast = Toast.makeText(getApplicationContext(),"Saved",Toast.LENGTH_SHORT);
+                    myToast.show();
+                }else
+                {
 
-    private boolean validateForm() {
-        boolean valid = true;
-
-        String uName = usernameField.getText().toString();
-        if (TextUtils.isEmpty(uName)) {
-            usernameField.setError("Required.");
-            valid = false;
-        } else {
-            usernameField.setError(null);
-        }
-
-        String pWord = passwordField.getText().toString();
-        if (TextUtils.isEmpty(pWord)) {
-            passwordField.setError("Required.");
-            valid = false;
-        } else {
-            passwordField.setError(null);
-        }
-
-        return valid;
-    }
-
-    private boolean validateForm2() {
-        boolean valid = true;
-
-        String uName = signUpEmailField.getText().toString();
-        if (TextUtils.isEmpty(uName)) {
-            signUpEmailField.setError("Required.");
-            valid = false;
-        } else {
-            signUpEmailField.setError(null);
-        }
-
-        String pWord = signUpPasswordField.getText().toString();
-        if (TextUtils.isEmpty(pWord)) {
-            signUpPasswordField.setError("Required.");
-            valid = false;
-        } else {
-            signUpPasswordField.setError(null);
-        }
-
-        return valid;
-    }
-
-    private void updateUI(FirebaseUser user) {
-        if (user != null) {
-            findViewById(R.id.alreadySignedInLayout).setVisibility(View.VISIBLE);
-            findViewById(R.id.signInLayout).setVisibility(View.GONE);
-            findViewById(R.id.signUpLayout).setVisibility(View.GONE);
-        } else {
-            findViewById(R.id.alreadySignedInLayout).setVisibility(View.GONE);
-            findViewById(R.id.signInLayout).setVisibility(View.VISIBLE);
-            findViewById(R.id.signUpLayout).setVisibility(View.GONE);
-        }
-    }
-
-    private void signOut() {
-        mAuth.signOut();
-        updateUI(null);
-    }
-
-    public void onClick(View view)
-    {
-        switch (view.getId())
-        {
-            case R.id.signInButton:
-                signIn(usernameField.getText().toString(), passwordField.getText().toString());
+                }
                 break;
-
-            case R.id.signUpButton:
-                findViewById(R.id.signInLayout).setVisibility(View.GONE);
-                findViewById(R.id.signUpLayout).setVisibility(View.VISIBLE);
-                break;
-
-            case R.id.continueButton:
-                //Intent loginSuccessIntent = new Intent(this,loginSuccess.class);
-                //startActivity(loginSuccessIntent);
-                break;
-
-            case R.id.signOutButton:
-                signOut();
-                break;
-
-            case R.id.authenticateButton:
-                createUser(signUpEmailField.getText().toString(), signUpPasswordField.getText().toString());
+            case R.id.clearButton:
+                clearFunc((ViewGroup) findViewById(R.id.scrollLayout));
                 break;
         }
+
     }
 }
