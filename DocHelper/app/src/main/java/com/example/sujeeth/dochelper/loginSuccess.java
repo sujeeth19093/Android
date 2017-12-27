6150394ab61987936417bc351590c2a50cbe5855
Sujeeth <sujeeth19093@gmail.com>
2017-12-26 17:24:07 -0800

    *implemented patient ID check

--- DocHelper/app/src/main/java/com/example/sujeeth/dochelper/loginSuccess.java
+++ DocHelper/app/src/main/java/com/example/sujeeth/dochelper/loginSuccess.java
@@ -8,21 +8,32 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class loginSuccess extends AppCompatActivity implements View.OnClickListener{
 
+    private DatabaseReference mDatabase;
+
     Button Go,addNew;
     EditText patientID;
-
+    TextView noPatientExists;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_success);
         Intent intent = getIntent();
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
         Go = (Button)findViewById(R.id.goButton);
         Go.setOnClickListener(this);
@@ -31,23 +42,43 @@ public class loginSuccess extends AppCompatActivity implements View.OnClickListe
 
         patientID = (EditText)findViewById(R.id.patientIDTxtBox);
         patientID.setInputType(InputType.TYPE_NUMBER_FLAG_DECIMAL);
+
+        noPatientExists = (TextView) findViewById(R.id.newPatientTxtV);
+    }
+
+    private void defaultUI()
+    {
+        patientID.setText("");
+        noPatientExists.setText("");
     }
 
     public void onClick(View v){
         switch(v.getId())
         {
             case R.id.goButton:
-                if(patientID.getText().toString().equals("010277891"))
-                {
-                    Intent goIntent = new Intent(this, patientInfo.class);
-                    startActivity(goIntent);
-                }else
-                {
+                mDatabase.child("patients").child(patientID.getText().toString()).addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if(dataSnapshot.exists())
+                        {
+                            Intent goIntent = new Intent(loginSuccess.this, patientInfo.class);
+                            defaultUI();
+                            startActivity(goIntent);
+                        }else
+                        {
+                            noPatientExists.setText("Patient does not exist, please add patient");
+                        }
+                    }
 
-                }
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
                 break;
             case R.id.addNewButton:
                 Intent addNewIntent = new Intent(this, addPatientInfo.class);
+                defaultUI();
                 startActivity(addNewIntent);
                 break;
         }
